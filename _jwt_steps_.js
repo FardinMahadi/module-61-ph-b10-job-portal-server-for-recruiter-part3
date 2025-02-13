da9681c236804d5692530970f1e29de658614005
FardinMahadi <mahadihasanfardin2015@gmail.com>
2025-02-14 02:47:04 +0600

    jwt added

---
+++ _jwt_steps_.js
@@ -0,0 +1,17 @@
+/**
+ * ----------------------------
+ *           JWT
+ * ----------------------------
+ *
+ * install jsonwebtoken cookie-parser
+ * set cookieParser as middleware
+ *
+ * 1. create a token
+ *
+ * jwt.sign(data, secret, { expiresIn: '5h' })
+ *
+ * set token to the cookie of res
+ *
+ * 2. send the token to the client side
+ *
+ */
